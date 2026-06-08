4607d28dbe6ae80cb1db82474e8145858b74d516
ThanhDinh <58394144+snacklun@users.noreply.github.com>
2026-06-08 15:19:14 +0000

    Sync LeetCode submission Runtime - 109 ms (57.97%), Memory - 36.8 MB (50.30%)

---
+++ DSA LeetCode/2211-k-radius-subarray-averages/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def getAverages(self, nums: List[int], k: int) -> List[int]:
+        
+        if k == 0:
+            return nums
+        
+        n = len(nums)
+        window_size = 2 * k + 1
+        result = [-1] * n
+        prefix = [0] * (n+1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+
+        for i in range(k, n - k):
+            leftBound, rightBound = i - k, i + k
+            subArraySum = prefix[rightBound + 1] - prefix[leftBound]
+            temp = subArraySum // window_size
+            result[i] = temp
+        
+        return result
+       
